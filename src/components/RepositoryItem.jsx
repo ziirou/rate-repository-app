3e00d37101c4392dcca00f6d3664091b5b2b509e
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-05-21 12:53:35 +0300

    Part 10b: Using flexbox for layout

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -2,13 +2,27 @@ import { View, StyleSheet } from 'react-native';
 import Text from './Text';
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 20,
+  flexContainer: {
+    display: 'flex',
+  },
+  flexItemA: {
+    flexGrow: 0,
+    backgroundColor: 'green',
+  },
+  flexItemB: {
+    flexGrow: 1,
+    backgroundColor: 'blue',
   },
 });
 
 const RepositoryItem = ({item}) => (
-  <View style={styles.container}>
+  <View style={styles.flexContainer}>
+    <View style={styles.flexItemA}>
+      <Text>Flex item A</Text>
+    </View>
+    <View style={styles.flexItemB}>
+      <Text>Flex item B</Text>
+    </View>
     <Text fontWeight="bold" fontSize="subheading">Full name: {item.fullName}</Text>
     <Text color="textSecondary">Description: {item.description}</Text>
     <Text color="primary">Language: {item.language}</Text>
