16f3461e1d26f318a0af52307057a8943aa53452
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-05-23 16:24:29 +0300

    Part 10c: Enhancing Apollo Client's requests

--- App.js
+++ App.js
@@ -3,7 +3,10 @@ import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/client';
 import Main from './src/components/Main';
 import createApolloClient from './src/utils/apolloClient';
-const apolloClient = createApolloClient();
+import AuthStorage from './src/utils/authStorage';
+
+const authStorage = new AuthStorage();
+const apolloClient = createApolloClient(authStorage);
 
 const App = () => {
   return (
--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,14 +1,31 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import Constants from 'expo-constants';
 
-const createApolloClient = () => {
-  const apolloUri = Constants.expoConfig.extra.apolloUri;
-  if (!apolloUri) {
-    throw new Error('Apollo URI is not defined in the environment variables');
-  }
+const { apolloUri } = Constants.expoConfig.extra;
+
+const httpLink = createHttpLink({
+  uri: apolloUri,
+});
+
+const createApolloClient = (authStorage) => {
+  const authLink = setContext(async (_, { headers }) => {
+    try {
+      const accessToken = await authStorage.getAccessToken();
+      return {
+        headers: {
+          ...headers,
+          authorization: accessToken ? `Bearer ${accessToken}` : '',
+        },
+      };
+    } catch (e) {
+      console.log(e);
+      return { headers };
+    }
+  });
 
   return new ApolloClient({
-    uri: apolloUri,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
   });
 };
