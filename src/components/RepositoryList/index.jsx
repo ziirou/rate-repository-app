34cd20d8e7625f029de52aceea5a404a4707f30e
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-05-26 10:34:02 +0300

    Part 10d: Handling dependencies in tests

--- src/components/RepositoryList/index.jsx
+++ src/components/RepositoryList/index.jsx
@@ -10,12 +10,10 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-const RepositoryList = () => {
-  const { repositories } = useRepositories();
-
+export const RepositoryListContainer = ({ repositories }) => {
   // Get the nodes from the edges array
   const repositoryNodes = repositories
-    ? repositories.edges.map(edge => edge.node)
+    ? repositories.edges.map((edge) => edge.node)
     : [];
 
   return (
@@ -25,6 +23,12 @@ const RepositoryList = () => {
       renderItem={({item}) => <RepositoryItem item={item} />}
     />
   );
+};
+
+const RepositoryList = () => {
+  const { repositories } = useRepositories();
+
+  return <RepositoryListContainer repositories={repositories} />;
 };
 
 export default RepositoryList;
